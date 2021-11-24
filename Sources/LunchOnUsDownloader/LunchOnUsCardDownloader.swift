559883f34ca7963d780a2b35a5e960349f89da27
Steffen Kötte <steffen.koette@gmail.com>
2021-11-23 19:09:30 -0800

    MacOS only

--- Sources/LunchOnUsDownloader/LunchOnUsCardDownloader.swift
+++ Sources/LunchOnUsDownloader/LunchOnUsCardDownloader.swift
@@ -1,7 +1,4 @@
 import Foundation
-#if canImport(FoundationNetworking)
-import FoundationNetworking
-#endif
 import SwiftBeanCountParserUtils
 
 /// Protocol to descibe an object which provides functions to download Lunch On Us Card data
